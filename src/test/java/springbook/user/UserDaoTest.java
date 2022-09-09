f6b8b0b061e867f87bd8a71f2bd39bfd3dcf8cae
Kevin LEE <hreeman.it@gmail.com>
2022-09-09 23:17:39 +0900

    feature: 스프링 테스트로 변경
    - JUnit5에서 사용했던 @BeforeAll은 JUnit4의 @BeforeClass와 대응되며 @Before는 @BeforeEach로 대응 됨. 책 예제에 맞춰 BeforeEach로 변경
    - SpringTest 라이브러리 의존성 추가
    - JUnit4의 @RunWith(SpringlUnit4ClassRunner.class)는 @ExtendWith(SpringExtension.class)로 변경 됨.
    - 책 예제에서는 XML기반의 설정으로 진행하나, 예제를 따라칠때는 @Configuration 기반으로 작업했으므로 해당 설정에 맞도록 진행

--- src/test/java/springbook/user/UserDaoTest.java
+++ src/test/java/springbook/user/UserDaoTest.java
@@ -1,11 +1,14 @@
 package springbook.user;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
@@ -19,80 +22,83 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 /**
  * 테스트를 위한 main 메서드를 실행 시키기 위한 클래스
  */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = DaoFactory.class)
 public class UserDaoTest {
-    private static UserDao userDao;
-    private static User user1;
-    private static User user2;
-    private static User user3;
+    @Autowired
+    private ApplicationContext context;
     
-    @BeforeAll
-    public static void setUp() {
-        final ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    private UserDao userDao;
+    private User user1;
+    private User user2;
+    private User user3;
+    
+    @BeforeEach
+    public void setUp() {
+        this.userDao = this.context.getBean("userDao", UserDao.class);
         
-        userDao = context.getBean("userDao", UserDao.class);
-        
-        user1 = new User("toby", "토비", "toby3");
-        user2 = new User("kimyh", "김영한", "kim1234");
-        user3 = new User("whiteship", "백기선", "white1234");
+        this.user1 = new User("toby", "토비", "toby3");
+        this.user2 = new User("kimyh", "김영한", "kim1234");
+        this.user3 = new User("whiteship", "백기선", "white1234");
     }
     
     @DisplayName("데이터 DB에 등록 후 조회한 결과와 등록한 결과가 일치 하는지 검사")
     @Test
     public void addAndGet() throws SQLException {
         //데이터 초기화
-        userDao.deleteAll();
+        this.userDao.deleteAll();
         
         //데이터 초기화 검증
-        assertThat(userDao.getCount(), is(0));
+        assertThat(this.userDao.getCount(), is(0));
      
         //데이터 등록
-        userDao.add(user1);
-        userDao.add(user2);
+        this.userDao.add(this.user1);
+        this.userDao.add(this.user2);
         
         //카운터 기능 검증
-        assertThat(userDao.getCount(), is(2));
+        assertThat(this.userDao.getCount(), is(2));
         
         //데이터 조회
-        final User findUser1 = userDao.get(user1.id());
+        final User findUser1 = this.userDao.get(this.user1.id());
     
         //테스트
-        assertThat(findUser1.name(), is(user1.name()));
-        assertThat(findUser1.password(), is(user1.password()));
+        assertThat(findUser1.name(), is(this.user1.name()));
+        assertThat(findUser1.password(), is(this.user1.password()));
         
         //데이터 조회
-        final User findUser2 = userDao.get(user2.id());
+        final User findUser2 = this.userDao.get(this.user2.id());
     
         //테스트
-        assertThat(findUser2.name(), is(user2.name()));
-        assertThat(findUser2.password(), is(user2.password()));
+        assertThat(findUser2.name(), is(this.user2.name()));
+        assertThat(findUser2.password(), is(this.user2.password()));
     }
     
     @DisplayName("DB 테이블의 레코드 수 조회 기능 검사")
     @Test
     public void count() throws SQLException {
-        userDao.deleteAll();
-        assertThat(userDao.getCount(), is(0));
+        this.userDao.deleteAll();
+        assertThat(this.userDao.getCount(), is(0));
     
-        userDao.add(user1);
-        assertThat(userDao.getCount(), is(1));
+        this.userDao.add(this.user1);
+        assertThat(this.userDao.getCount(), is(1));
     
-        userDao.add(user2);
-        assertThat(userDao.getCount(), is(2));
+        this.userDao.add(this.user2);
+        assertThat(this.userDao.getCount(), is(2));
     
-        userDao.add(user3);
-        assertThat(userDao.getCount(), is(3));
+        this.userDao.add(this.user3);
+        assertThat(this.userDao.getCount(), is(3));
     }
     
     @DisplayName("없는 사용자 조회시 예외 발생 검사")
     @Test
     public void getUserFailure() throws SQLException {
         //데이터 초기화
-        userDao.deleteAll();
+        this.userDao.deleteAll();
         
-        assertThat(userDao.getCount(), is(0));
+        assertThat(this.userDao.getCount(), is(0));
     
         assertThrows(EmptyResultDataAccessException.class, () -> {
-            userDao.get("unkown_id");
+            this.userDao.get("unkown_id");
         });
     }
 }
