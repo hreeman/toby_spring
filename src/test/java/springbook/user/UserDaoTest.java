f6370916d68bb93385393e28f77cbc61b7bdd73e
Kevin LEE <hreeman.it@gmail.com>
2022-09-09 21:55:47 +0900

    feature: Junit5를 활용한 테스트 코드
    - 테스트 경로로 위치 변경
    - 토비의 스프링 3.1책에 있는 테스트 검증 방법을 사용하기 위한 hamcrest 라이브러리 추가
    - 테스트 코드 JUnit으로 변경

---
+++ src/test/java/springbook/user/UserDaoTest.java
@@ -0,0 +1,43 @@
+package springbook.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springbook.user.dao.DaoFactory;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * 테스트를 위한 main 메서드를 실행 시키기 위한 클래스
+ */
+public class UserDaoTest {
+    @DisplayName("데이터 DB에 등록 후 조회한 결과와 등록한 결과가 일치 하는지 검사")
+    @Test
+    public void addAndGet() throws SQLException {
+        //테스트 할 UserDao 빈 획득
+        final ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        final UserDao userDao = context.getBean("userDao", UserDao.class);
+        
+        //등록할 데이터 생성
+        final User user = new User("toby", "토비", "toby3");
+        
+        //데이터 초기화
+        userDao.remove(user.id());
+        
+        //데이터 등록
+        userDao.add(user);
+        
+        //데이터 조회
+        final User findUser = userDao.get(user.id());
+    
+        //테스트
+        assertThat(findUser.name(), is(user.name()));
+        assertThat(findUser.password(), is(user.password()));
+    }
+}
