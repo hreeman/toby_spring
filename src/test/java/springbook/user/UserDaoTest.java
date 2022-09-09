599357a2f81a8a7348b7c753d4af987a7de7d5b4
Kevin LEE <hreeman.it@gmail.com>
2022-09-09 22:50:32 +0900

    feature: 테스트코드 리팩토링
    - 반복적으로 사용되는 UserDao 빈을 조회하기 위한 ApplicationContext를 통한 빈 획득 과정 추출
    - JUnit5에 맞춰 static인 @BeforeAll을 사용

--- src/test/java/springbook/user/UserDaoTest.java
+++ src/test/java/springbook/user/UserDaoTest.java
@@ -1,5 +1,6 @@
 package springbook.user;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -19,13 +20,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * 테스트를 위한 main 메서드를 실행 시키기 위한 클래스
  */
 public class UserDaoTest {
+    private static UserDao userDao;
+    
+    @BeforeAll
+    public static void setUp() {
+        final ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        
+        userDao = context.getBean("userDao", UserDao.class);
+    }
+    
     @DisplayName("데이터 DB에 등록 후 조회한 결과와 등록한 결과가 일치 하는지 검사")
     @Test
     public void addAndGet() throws SQLException {
-        //테스트 할 UserDao 빈 획득
-        final ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        final UserDao userDao = context.getBean("userDao", UserDao.class);
-        
         //데이터 초기화
         userDao.deleteAll();
         
@@ -61,23 +67,19 @@ public class UserDaoTest {
     @DisplayName("DB 테이블의 레코드 수 조회 기능 검사")
     @Test
     public void count() throws SQLException {
-        final ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        
-        final UserDao userDao = context.getBean("userDao", UserDao.class);
-        
         final User user1 = new User("gyumee", "박성철", "park1234");
         final User user2 = new User("kimyh", "김영한", "kim1234");
         final User user3 = new User("whiteship", "백기선", "white1234");
     
         userDao.deleteAll();
         assertThat(userDao.getCount(), is(0));
-        
+    
         userDao.add(user1);
         assertThat(userDao.getCount(), is(1));
-        
+    
         userDao.add(user2);
         assertThat(userDao.getCount(), is(2));
-        
+    
         userDao.add(user3);
         assertThat(userDao.getCount(), is(3));
     }
@@ -85,10 +87,6 @@ public class UserDaoTest {
     @DisplayName("없는 사용자 조회시 예외 발생 검사")
     @Test
     public void getUserFailure() throws SQLException {
-        final ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        
-        final UserDao userDao = context.getBean("userDao", UserDao.class);
-        
         //데이터 초기화
         userDao.deleteAll();
         
