0163ac2e005b54541792752e52481dfa9a69f936
Kevin LEE <hreeman.it@gmail.com>
2022-09-10 19:33:28 +0900

    feature: SpringTest 테스트
    - SpringTest를 이용시 정말 ApplicationContext를 한번 생성하고 재사용하는지에 대한 테스트 코드 추가
    - 다양한 JUnit의 테스트 방법 학습 테스트 추가

--- src/test/java/springbook/learningtest/junit/JUnitTest.java
+++ src/test/java/springbook/learningtest/junit/JUnitTest.java
@@ -1,40 +1,73 @@
 package springbook.learningtest.junit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.hamcrest.CoreMatchers.either;
 import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * JUnit 자체 테스트
+ * JUnit 자체 테스트 / Spring Test 테스트
  *
  * <pre>
- * JUnit이 테스트 메소드를 수행할 때 마다 새로운 오브젝트를 생성한다고 하는데
+ * 1. JUnit이 테스트 메소드를 수행할 때 마다 새로운 오브젝트를 생성한다고 하는데
+ * 진짜 그러한가?를 위한 테스트
+ *
+ * 2. Spring Test의 경우 테스트시 매번 동일한 어플리케이션 컨텍스트를 재사용한다는데
  * 진짜 그러한가?를 위한 테스트
  * </pre>
  */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = JUnitTest.JUnit.class)
 public class JUnitTest {
+    @Autowired
+    private ApplicationContext context;
+    
     static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
     
     @Test
     public void test1() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+        
+        assertThat((contextObject == null || contextObject == this.context), is(true));
+        contextObject = this.context;
     }
     
     @Test
     public void test2() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+    
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
     
     @Test
     public void test3() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+    
+        assertThat(contextObject, either(is(nullValue())).or(is(this.context)));
+        contextObject = this.context;
+    }
+    
+    @Configuration
+    static class JUnit {
+    
     }
 }
