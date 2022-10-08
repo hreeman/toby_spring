15cf5b0a9a90da9941a2eec199f2cbdc71750485
Kevin LEE <hreeman.it@gmail.com>
2022-10-08 14:40:09 +0900

    test: 사용자 클래스에 대한 로직 추가에 따른 테스트 추가
    - 사용자 클래스에서 레벨 업그레이드 로직에 대한 책임을 가져감에 따른 해당 로직 테스트 코드 추가

---
+++ src/test/java/springbook/user/domain/UserTest.java
@@ -0,0 +1,65 @@
+package springbook.user.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 사용자 클래스에 추가된 로직에 대한 테스트
+ */
+class UserTest {
+    @DisplayName("사용자 레벨 업그레이드 테스트")
+    @Test
+    void upgradeLevel() {
+        final Level[] levels = Level.values();
+        
+        for (final Level level : levels) {
+            if (level.nextLevel() == null) {
+                continue;
+            }
+            
+            // Given
+            final User user = new User(
+                    "id",
+                    "name",
+                    "password",
+                    level,
+                    0,
+                    0
+            );
+            
+            // When
+            final User upgradeUser = user.upgradeLevel();
+            
+            // Then
+            assertThat(upgradeUser.level()).isEqualTo(level.nextLevel());
+        }
+    }
+    
+    @DisplayName("업그레이드 불가 테스트")
+    @Test
+    public void cannotUpgradeLevel() {
+        final Level[] levels = Level.values();
+    
+        for (final Level level : levels) {
+            if (level.nextLevel() != null) {
+                continue;
+            }
+        
+            // Given
+            final User user = new User(
+                    "id",
+                    "name",
+                    "password",
+                    level,
+                    0,
+                    0
+            );
+        
+            // When & Then
+            Assertions.assertThrows(IllegalStateException.class, user::upgradeLevel);
+        }
+    }
+}
